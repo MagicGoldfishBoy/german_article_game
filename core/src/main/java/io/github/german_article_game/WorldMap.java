df73d5c4c97f892853b3708d0b7adb51d950254f
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2026-07-05 10:30:51 -0400

    added worldmap class

---
+++ core/src/main/java/io/github/german_article_game/WorldMap.java
@@ -0,0 +1,49 @@
+package io.github.german_article_game;
+
+import com.badlogic.gdx.Screen;
+
+public class WorldMap implements Screen {
+
+    public static Player player;
+
+    @Override
+    public void show() {
+        player = new Player();
+    }
+
+    @Override
+    public void render(float delta) {
+        player.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resize'");
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pause'");
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resume'");
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hide'");
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dispose'");
+    }
+    
+}
